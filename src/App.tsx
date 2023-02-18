83fca6dabf4ec0f89103cd9a047a2b11f2e40b61
LEE DONG YEONG <kinzinzz@naver.com>
2023-02-18 23:48:41 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Day from "./components/Day";
+import DayList from "./components/DayList";
+import Header from "./components/Header";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import EmptyPage from "./components/EmptyPage";
+import CreateWord from "./components/CreateWord";
+import CreateDay from "./components/CreateDay";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" >
+            <DayList />
+          </Route>
+          <Route path="/day/:day">
+            <Day />
+          </Route>
+          <Route path="/create_word">
+            <CreateWord />
+          </Route>
+          <Route path="/create_day">
+            <CreateDay />
+          </Route>
+          <Route>
+            <EmptyPage />
+          </Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
